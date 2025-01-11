4d266d4a2d9fbec107930438ef57bebaa4fb1ddb
dheerajyadav1468 <dheerajyadav1468@gmail.com>
2025-01-11 14:07:00 +0530

    dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
 import "jsvectormap/dist/css/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
-import "@/css/satoshi.css";
-import "@/css/style.css";
+import "../css/satoshi.css";
+import "../css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "../components/common/Loader";
 
