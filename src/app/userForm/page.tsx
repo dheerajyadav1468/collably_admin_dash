8796670fdf5d8b3d103e85b63069fc4b4b618813
dheerajyadav1468 <dheerajyadav1468@gmail.com>
2025-02-11 09:40:21 +0530

    admin deploy1

--- src/app/userForm/page.tsx
+++ src/app/userForm/page.tsx
@@ -1,31 +1,15 @@
-"use client"; 
+"use client";
 
 import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
 import UserForm from "../../components/ProfileBox/userform";
-import { Metadata } from "next";
 import DefaultLayout from "../../components/Layouts/DefaultLaout";
-
-type ProductFormData = {
-  name: string;
-    brandLogo: File | null;
-    brandname: string;
-    description: string;
-    price: number;
-    quantity: number;
-    category: string;
-    status: string;
-  };
-
-const handleSubmit = (formData: ProductFormData) => {
-  console.log(formData);
-};
 
 const TablesPage = () => {
   return (
     <DefaultLayout>
-      <Breadcrumb pageName="Tables" />
+      <Breadcrumb pageName="User Form" />
       <div className="flex flex-col gap-10">
-        <UserForm onSubmit={handleSubmit} />
+        <UserForm />
       </div>
     </DefaultLayout>
   );
