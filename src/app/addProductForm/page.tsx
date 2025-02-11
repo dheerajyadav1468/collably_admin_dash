615530c17e6f7235c7691b942f00e4b108dfadf3
dheerajyadav1468 <dheerajyadav1468@gmail.com>
2025-02-11 09:26:54 +0530

    admin deploy1

--- src/app/addProductForm/page.tsx
+++ src/app/addProductForm/page.tsx
@@ -4,27 +4,12 @@ import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
 import ProductForm from "../../components/ProfileBox/productForm";
 import DefaultLayoutBrand from "../../components/Layouts/DefaultLayoutBrand";
 
-type ProductFormData = {
-  name: string;
-  brandLogo: File | null;
-  brandname: string;
-  description: string;
-  price: number;
-  quantity: number;
-  category: string;
-  status: string;
-};
-
 const TablesPage = () => {
-  const handleSubmit = (formData: ProductFormData) => {
-    console.log("Product Submitted:", formData);
-  };
-
   return (
     <DefaultLayoutBrand>
       <Breadcrumb pageName="Tables" />
       <div className="flex flex-col gap-10">
-        <ProductForm onSubmit={handleSubmit} />
+        <ProductForm />
       </div>
     </DefaultLayoutBrand>
   );
