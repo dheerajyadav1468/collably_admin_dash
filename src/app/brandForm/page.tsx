32b9afcb4805c42d34545336de3be2c3cae5e980
dheerajyadav1468 <dheerajyadav1468@gmail.com>
2025-02-11 09:29:51 +0530

    admin deploy

--- src/app/brandForm/page.tsx
+++ src/app/brandForm/page.tsx
@@ -2,31 +2,14 @@
 
 import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
 import BrandOnboardingForm from "../../components/ProfileBox/brandform";
-import { Metadata } from "next";
 import DefaultLayout from "../../components/Layouts/DefaultLaout";
-
-type BrandFormData = {
-    brandName: string;
-    brandLogo: File | null;
-    brandDescription: string;
-    brandCategory: string;
-    contactEmail: string;
-    brandWebsite: string;
-    brandPhone: string;
-    socialMediaLinks: string;
-    gstNumber: string;
-  };
-
-const handleSubmit = (formData: BrandFormData) => {
-  console.log(formData);
-};
 
 const TablesPage = () => {
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Tables" />
       <div className="flex flex-col gap-10">
-        <BrandOnboardingForm onSubmit={handleSubmit} />
+        <BrandOnboardingForm />
       </div>
     </DefaultLayout>
   );
